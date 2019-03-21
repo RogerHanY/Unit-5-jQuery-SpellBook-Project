07536706d1280773eaa534e49281be0ea3098a7f
Roger Han <47434976+RogerHanY@users.noreply.github.com>
2019-03-21 14:51:23 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,34 @@
+//Spell #1
+
+$(".c").click(function(){
+ $(".p").delay("1000").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$(".a").click(function(){
+ $(".i").toggle();
+});
+
+
+
+//Spell #3
+
+$(".t").click(function(){
+ $(".g").delay("2000").slideToggle();
+});
+
+
+//Spell #4
+
+$(".k").mouseover(function(){
+ $(".h").fadeIn();
+});
+
+$(".k").mouseout(function(){
+ $(".h").fadeOut();
+});
+
+
